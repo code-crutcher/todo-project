b988c4d4cec4db7f8d9efef771e082656e0fb934
code-crutcher <swethapratap23@gmail.com>
2026-01-02 15:40:18 +0530

    frontend setup complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage.jsx'
+import CreatePage from './pages/CreatePage.jsx'
+import NoteDetailPage from './pages/NoteDetailPage.jsx'
+import UserDetailPage from './pages/UserDetailPage.jsx'
+// import toast from 'react-hot-toast'
+
+const App = () => {
+  return (
+    <div data-theme="cupcake">
+      
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="/create"
+          element={<CreatePage />}
+        />
+        <Route
+          path="/note/:id"
+          element={<NoteDetailPage />}
+        />
+        <Route
+          path="/user/:id"
+          element={<UserDetailPage />}
+        />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
