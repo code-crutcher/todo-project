a1b4fb82be5305711680fa4a67cdf36cadacce1e
code-crutcher <swethapratap23@gmail.com>
2026-01-02 16:17:36 +0530

    ratelimiter design completed

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React , {useState} from 'react'
 import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI'
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(true)
   return (
     <div className='min-h-screen'>
       <Navbar/>
+      { isRateLimited && <RateLimitedUI/>}
     </div>
   )
 }
